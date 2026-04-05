3bf7a5fea26b5838ab8b6086d5e21422ee941c5b
kasta-sports-tv <kasta.sports.tv@gmail.com>
2026-04-05 23:40:36 +0300

    Refactor ffmpeg execution to use spawn method

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 import express from "express";
-import { exec } from "child_process";
+import { spawn } from "child_process";
 import fs from "fs";
 
 const app = express();
 
 const HLS_DIR = "./hls";
 
-// створюємо папку якщо нема
 if (!fs.existsSync(HLS_DIR)) {
   fs.mkdirSync(HLS_DIR);
 }
 
-// 🔥 тут твій стрім
-const STREAM_URL = "http://livego.club:8080/live/.../281096.ts";
+// ⚠️ ВСТАВ СВІЙ ПОВНИЙ TS ЛІНК ТУТ
+const STREAM_URL = "http://livego.club:8080/live/ТУТ_ПОВНИЙ_ЛІНК.ts";
 
-// запускаємо ffmpeg
-exec(`ffmpeg -i "${STREAM_URL}" -c copy -f hls -hls_time 2 -hls_list_size 6 -hls_flags delete_segments ${HLS_DIR}/index.m3u8`);
+// 👇 запускаємо ffmpeg (вбудований в систему Render)
+const ffmpeg = spawn("ffmpeg", [
+  "-i", STREAM_URL,
+  "-c", "copy",
+  "-f", "hls",
+  "-hls_time", "2",
+  "-hls_list_size", "6",
+  "-hls_flags", "delete_segments",
+  `${HLS_DIR}/index.m3u8`
+]);
+
+ffmpeg.stderr.on("data", (data) => {
+  console.log("FFmpeg:", data.toString());
+});
+
+ffmpeg.on("close", (code) => {
+  console.log("FFmpeg stopped:", code);
+});
 
 app.use("/hls", express.static(HLS_DIR));
-
-app.get("/", (req, res) => {
-  res.send("HLS працює 😏");
-});
 
 app.listen(3000, () => {
   console.log("Server started");
